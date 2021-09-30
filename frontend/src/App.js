10b364ecb731565f734d063020e84e73b0d1aa20
Aryan Garg <garg2001aryan@gmail.com>
2021-09-30 19:09:41 +0530

    2 Templates Created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import { Template1 } from "./Templates/Template1/Template1";
-import { Template2 } from "./Templates/Template2/Template2";
+import {Template1} from "./Templates/Template1/Template1";
+import {Template2} from "./Templates/Template2/Template2";
+import {Login} from "./Components/Login/Login";
+
+import {Header} from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      {/* <Template1 /> */}
-      <Template2 />
+      {/* <Header /> */}
+      {/* <Login /> */}
+      <Template1 />
+      {/* <Template2 /> */}
     </div>
   );
 }
